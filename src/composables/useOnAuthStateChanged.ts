54ab9f2e9e86d45695fa9fbafd8b6d61ddf3566f
yuyasat <takiyour@gmail.com>
2023-05-25 11:07:39 +0900

    fix handling when user is blank

--- src/composables/useOnAuthStateChanged.ts
+++ src/composables/useOnAuthStateChanged.ts
@@ -5,12 +5,11 @@ import router from "@/router";
 const useOnAuthStateChanged = (store: any, afterFunction: () => any) => {
   auth.onAuthStateChanged(async (user) => {
     if (user) {
-      console.log(user);
       store.commit("setFirebaseUser", user);
       afterFunction();
       return user
     } else {
-      router.push("/login");
+      router.push("/");
     }
   })
 }
